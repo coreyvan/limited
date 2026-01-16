f02fcd89f3361c9f4afe528a8e4042829f2b10cb
Corey Van Woert <corey@telegraph.io>
2026-01-16 15:07:48 -0500

    more features

--- examples/client/main.go
+++ examples/client/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"slices"
+	"syscall"
+	"time"
+
+	"go.opentelemetry.io/contrib/instrumentation/runtime"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.38.0"
 
 	"github.com/coreyvan/limited"
 )
@@ -21,52 +33,79 @@ func main() {
 		MaxRetries: 5,
 	})
 
-	//if err := l.Call(func() error {
-	//	req, err := http.NewRequest(http.MethodGet, "http://localhost:8080/toomany", nil)
-	//	if err != nil {
-	//		return fmt.Errorf("failed to create request: %w", err)
-	//	}
-	//
-	//	resp, err := http.DefaultClient.Do(req)
-	//	if err != nil {
-	//		return fmt.Errorf("request failed: %w", err)
-	//	}
-	//	defer resp.Body.Close()
-	//
-	//	if slices.Contains(retryableStatusCodes, resp.StatusCode) {
-	//		return limited.WrapRetryable(fmt.Errorf("received retryable status code: %d", resp.StatusCode))
-	//	}
-	//	fmt.Printf("Request succeeded with status code: %s\n", resp.Status)
-	//	return nil
-	//}); err != nil {
-	//	panic(err)
-	//}
+	err := setupOpenTelemetry()
+	if err != nil {
+		log.Fatalf("Failed to setup OpenTelemetry exporter: %v", err)
+	}
+	log.Println("otel setup successfully")
 
-	for i := 0; i < 10; i++ {
-		if err := l.Call(func() error {
-			req, err := http.NewRequest(http.MethodGet, "http://localhost:8080/bucket", nil)
-			if err != nil {
-				return fmt.Errorf("failed to create request: %w", err)
-			}
+	ticker := time.NewTicker(300 * time.Millisecond)
 
-			resp, err := http.DefaultClient.Do(req)
-			if err != nil {
-				return fmt.Errorf("request failed: %w", err)
-			}
-			defer resp.Body.Close()
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	errChan := make(chan error, 1)
 
-			b, err := io.ReadAll(resp.Body)
-			if err != nil {
-				return fmt.Errorf("failed to read response body: %w", err)
-			}
-			log.Printf("Got response body: %s", string(b))
+	for {
+		select {
+		case <-ticker.C:
+			log.Println("tick... making request to server")
+			if err := l.Call(func() error {
+				req, err := http.NewRequest(http.MethodGet, "http://localhost:8080/bucket", nil)
+				if err != nil {
+					return fmt.Errorf("failed to create request: %w", err)
+				}
 
-			if slices.Contains(retryableStatusCodes, resp.StatusCode) {
-				return limited.WrapRetryable(fmt.Errorf("received retryable status code: %d", resp.StatusCode))
+				resp, err := http.DefaultClient.Do(req)
+				if err != nil {
+					return fmt.Errorf("request failed: %w", err)
+				}
+				defer resp.Body.Close()
+
+				b, err := io.ReadAll(resp.Body)
+				if err != nil {
+					return fmt.Errorf("failed to read response body: %w", err)
+				}
+				log.Printf("Got response body: %s", string(b))
+
+				if slices.Contains(retryableStatusCodes, resp.StatusCode) {
+					return limited.WrapRetryable(fmt.Errorf("received retryable status code: %d", resp.StatusCode))
+				}
+				return nil
+			}); err != nil {
+				errChan <- err
 			}
-			return nil
-		}); err != nil {
-			panic(err)
+		case sig := <-sigChan:
+			log.Printf("Received signal, initiating shutdown... signal: %v", sig)
+		case err := <-errChan:
+			log.Fatalf("Received an error: %v", err)
 		}
 	}
+}
+
+func setupOpenTelemetry() error {
+	ctx := context.Background()
+	exp, err := otlpmetricgrpc.New(ctx, otlpmetricgrpc.WithInsecure(), otlpmetricgrpc.WithEndpointURL("http://localhost:4317"))
+	if err != nil {
+		return err
+	}
+
+	reader := sdkmetric.NewPeriodicReader(exp, sdkmetric.WithInterval(30*time.Second))
+
+	res, err := resource.New(ctx,
+		resource.WithAttributes(
+			semconv.DeploymentEnvironmentName("limited.development.server"),
+		),
+	)
+
+	mp := sdkmetric.NewMeterProvider(sdkmetric.WithReader(reader), sdkmetric.WithResource(res))
+
+	otel.SetMeterProvider(mp)
+
+	go func() {
+		if err := runtime.Start(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	return nil
 }
--- examples/server/main.go
+++ examples/server/main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"context"
 	"log"
+	"math/rand/v2"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	otelchimetric "github.com/riandyrn/otelchi/metric"
+	"go.opentelemetry.io/contrib/instrumentation/runtime"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.38.0"
 
 	"github.com/coreyvan/limited"
 )
@@ -23,7 +36,19 @@ func main() {
 	defer func() {
 		_ = sl.Stop()
 	}()
+	err = setupOpenTelemetry()
+	if err != nil {
+		log.Fatalf("Failed to setup OpenTelemetry exporter: %v", err)
+	}
+	log.Println("otel setup successfully")
+
+	baseCfg := otelchimetric.NewBaseConfig("limited-server", otelchimetric.WithMeterProvider(otel.GetMeterProvider()))
 	r := chi.NewRouter()
+	r.Use(
+		otelchimetric.NewRequestDurationMillis(baseCfg),
+		otelchimetric.NewRequestInFlight(baseCfg),
+		otelchimetric.NewResponseSizeBytes(baseCfg),
+	)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
@@ -31,6 +56,8 @@ func main() {
 		w.WriteHeader(http.StatusTooManyRequests)
 	})
 	r.Get("/bucket", func(w http.ResponseWriter, r *http.Request) {
+		jitter := time.Duration(50+rand.IntN(200)) * time.Millisecond
+		time.Sleep(jitter)
 		if !sl.Allow() {
 			log.Printf("Too Many Requests")
 			w.WriteHeader(http.StatusTooManyRequests)
@@ -46,9 +73,54 @@ func main() {
 		Handler: r,
 	}
 
-	log.Println("Listening on", s.Addr)
-	err = s.ListenAndServe()
-	if err != nil {
-		log.Fatal(err)
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	errChan := make(chan error, 1)
+
+	go func() {
+		log.Println("Listening on", s.Addr)
+		errChan <- s.ListenAndServe()
+	}()
+
+	select {
+	case sig := <-sigChan:
+		log.Printf("Received signal, initiating shutdown... signal: %v", sig)
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		if err := s.Shutdown(ctx); err != nil {
+			log.Printf("Error during server shutdown: %v", err)
+		} else {
+			log.Println("Server shutdown gracefully")
+		}
+	case err := <-errChan:
+		log.Fatalf("Server encountered an error: %v", err)
 	}
+}
+
+func setupOpenTelemetry() error {
+	ctx := context.Background()
+	exp, err := otlpmetricgrpc.New(ctx, otlpmetricgrpc.WithInsecure(), otlpmetricgrpc.WithEndpointURL("http://localhost:4317"))
+	if err != nil {
+		return err
+	}
+
+	reader := sdkmetric.NewPeriodicReader(exp, sdkmetric.WithInterval(30*time.Second))
+
+	res, err := resource.New(ctx,
+		resource.WithAttributes(
+			semconv.DeploymentEnvironmentName("limited.development.server"),
+		),
+	)
+
+	mp := sdkmetric.NewMeterProvider(sdkmetric.WithReader(reader), sdkmetric.WithResource(res))
+
+	otel.SetMeterProvider(mp)
+
+	go func() {
+		if err := runtime.Start(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	return nil
 }
